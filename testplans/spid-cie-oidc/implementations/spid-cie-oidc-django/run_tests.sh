e19b59ea62bf9ca4f2fae2187f905da66f466a05
Roberto <roberto.savi2582@gmail.com>
2024-07-09 11:27:12 +0200

    Update run_tests.sh

--- testplans/spid-cie-oidc/implementations/spid-cie-oidc-django/run_tests.sh
+++ testplans/spid-cie-oidc/implementations/spid-cie-oidc-django/run_tests.sh
@@ -10,19 +10,26 @@ fi
 SESSION_CONTENT=$(<"$1")
 TEST_CONTENT=$(<"$2")
 
+# Print session content
+echo "$SESSION_CONTENT"
+echo    # Print a blank line
+
+# Print test content
+echo "$TEST_CONTENT"
+
 # Generate random username and password
 USERNAME=$(openssl rand -hex 8)
 PASSWORD=$(openssl rand -hex 8)
 
 # Register the user
 echo "Registering user..."
-curl -X POST http://localhost:3000/users \
+curl -X POST http://burpsuite:3000/users \
   -H "Content-Type: application/json" \
   -d "{\"name\": \"$USERNAME\", \"password\": \"$PASSWORD\"}"
 
 # Login and capture the token
 echo "Logging in and capturing token..."
-RESPONSE=$(curl -s -X POST http://localhost:3000/users/login \
+RESPONSE=$(curl -s -X POST http://burpsuite:3000/users/login \
   -H "Content-Type: application/json" \
   -d "{\"name\": \"$USERNAME\", \"password\": \"$PASSWORD\"}")
 
@@ -31,8 +38,8 @@ TOKEN=$(echo "$RESPONSE" | grep -o '"token": *"[^"]*' | grep -o '[^"]*$')
 
 # Sending session and test and capturing output
 echo "Sending session and test and capturing output..."
-curl -X POST http://localhost:3000/send_message \
+curl -X POST http://burpsuite:3000/send_message \
 -H "Content-Type: text/plain" \
 -H "Authorization: Bearer $TOKEN" \
--d "$SESSION_CONTENT&$TEST_CONTENT" > output.txt
+-d "$SESSION_CONTENT&$TEST_CONTENT" > /output.txt
 echo "Output saved to output.txt"
